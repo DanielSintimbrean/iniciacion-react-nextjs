3fc7f408f51d9182b442521ac6abc7e85a3a25a9
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-27 17:07:23 +0100

    04 - useState

---
+++ src/components/counter.tsx
@@ -0,0 +1,25 @@
+/**
+ * Utilizar useState para crear un contador
+ *
+ * Ejercicio:
+ *   - Añadir "use client" y el import de useState
+ *   - Crear un estado count con valor inicial 0
+ *   - Crear un botón que incremente el contador al hacer clic
+ *   - Añadir un botón "Reset" que ponga el contador a 0
+ *   - El botón Reset debe estar deshabilitado si el contador ya es 0
+ */
+
+export function Counter() {
+  // TODO: Crear el estado usando useState
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2">
+      {/* TODO: Mostrar el valor del contador */}
+
+      {/* TODO: Botón para incrementar el contador */}
+
+      {/* TODO: Botón para resetear el contador */}
+    </div>
+  );
+}
+
