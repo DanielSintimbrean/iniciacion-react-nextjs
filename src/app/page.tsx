cfd6ce1ddadef4326db2fd37942c78c94f7e41f1
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-27 13:53:41 +0100

    03 - Parametros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white">
+        {/**
+         * - Utiliza el componente con diferentes textos y variants
+         */}
         <Button />
       </main>
     </div>
