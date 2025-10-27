7b371e2269aa9adcaaf45ea083c5eac66ad505ae
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-27 13:40:05 +0100

    02 - Componentes Final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
+import { Button } from "../components/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white">
-        Iniciación a React y Next.js
+        <Button />
       </main>
     </div>
   );
 }
-
-/**
- * Crear tu primer componente botón aquí
- *
- * Un componente es una función que cumple 2 requisitos:
- *   1. El nombre del componente debe empezar por mayúscula
- *   2. Devolver un elemento (tag HTML o un componente)
- *
- * Ejercicio:
- *   Crear un componente Button que devuelva un botón con el texto "Mi primer botón"
- */
-function 
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,3 +6,17 @@
  *   Copiar el componente Button que hemos creado en el archivo `page.tsx` y
  *   exportarlo para que podamos utilizarlo desde otro archivo.
  */
+
+/**
+ * Crear tu primer componente botón aquí
+ *
+ * Un componente es una función que cumple 2 requisitos:
+ *   1. El nombre del componente debe empezar por mayúscula
+ *   2. Devolver un elemento (tag HTML o un componente)
+ *
+ * Ejercicio:
+ *   Crear un componente Button que devuelva un botón con el texto "Mi primer botón"
+ */
+export function Button() {
+  return <button>Mi primer botón</button>;
+}
