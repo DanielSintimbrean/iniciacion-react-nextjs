e11cf467564497c1da6ff641bf3f317ba8209a4d
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-27 12:36:29 +0100

    02 - Componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,3 +7,15 @@ export default function Home() {
     </div>
   );
 }
+
+/**
+ * Crear tu primer componente botón aquí
+ *
+ * Un componente es una función que cumple 2 requisitos:
+ *   1. El nombre del componente debe empezar por mayúscula
+ *   2. Devolver un elemento (tag HTML o un componente)
+ *
+ * Ejercicio:
+ *   Crear un componente Button que devuelva un botón con el texto "Mi primer botón"
+ */
+function 
---
+++ src/components/button.tsx
@@ -0,0 +1,8 @@
+/**
+ * Para que podamos utilizar un componente desde otro archivo, debemos exportarlo
+ * utilizando la palabra reservada `export`.
+ *
+ * Ejercicio:
+ *   Copiar el componente Button que hemos creado en el archivo `page.tsx` y
+ *   exportarlo para que podamos utilizarlo desde otro archivo.
+ */
