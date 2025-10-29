b83b61c7d9a316f181b9e3595a0e1aa48c03f5e0
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-29 17:39:05 +0100

    06 - Enrutamiento Final

--- src/app/03-use-effect/page.tsx
+++ src/app/03-use-effect/page.tsx
@@ -1,1 +1,11 @@
+import { Counter } from "../../components/persistent-counter";
 
+export default function ElQueMeDaLaGana() {
+  return (
+    <div className="flex min-h-screen justify-center bg-zinc-50 font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white">
+        <Counter />
+      </main>
+    </div>
+  );
+}
