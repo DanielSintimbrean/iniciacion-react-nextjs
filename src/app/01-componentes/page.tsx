45e43e7b09dbaed7a0bce7c3e0d6e86020d3884c
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-29 17:39:05 +0100

    06 - Enrutamiento Final

--- src/app/01-componentes/page.tsx
+++ src/app/01-componentes/page.tsx
@@ -1,1 +1,13 @@
+import { Button } from "../../components/button";
 
+export default function ElQueMeDaLaGana() {
+  return (
+    <div className="flex min-h-screen justify-center bg-zinc-50 font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white">
+        <Button>Botón 1</Button>
+        <Button variant="red">Botón 2</Button>
+        <Button variant="gray">Botón 3</Button>
+      </main>
+    </div>
+  );
+}
