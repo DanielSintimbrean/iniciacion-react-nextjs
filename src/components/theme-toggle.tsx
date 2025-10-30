d149ae799dcb0789343cf7b2ab27eb3e4b8fdb2d
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-10-30 16:59:11 +0100

    09 - Context API

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+// import { useTheme } from "./theme-context";
+
+export function ThemeToggle() {
+  // - usa el hook `useTheme`
+  // - Al hacer click, cambia entre "light" y "dark"
+  return <button>Toggle theme</button>;
+}
